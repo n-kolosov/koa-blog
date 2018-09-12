94ca869cee8102ebe682c4ac6ba5d6d47a8a2375
Nikolay <nrf-224@mail.ru>
2018-09-12 22:58:28 +0300

    Second commit

--- app/server.js
+++ app/server.js
@@ -1,6 +1,7 @@
 import app from './app';
+import { PORT } from './config.js';
 
-const PORT = process.env.PORT || 4000;
+//const PORT = process.env.PORT || 4000;
 
 const server = app.listen(PORT, (err) => {
   if (err) console.log(err);
